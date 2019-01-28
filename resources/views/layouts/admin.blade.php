b04a7cf9ac9755342dd8880b7774edd9265c01d8
Satendra Maurya <satendramaurya@Satendra1000170-MPR00060.local>
2019-01-29 02:05:48 +0530

    test has done

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,8 +22,8 @@
             <nav class="navbar navbar-static-top">
                 <div class="container">
                     <div class="navbar-header">
-                        <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" class="navbar-brand">
-                            {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+                        <a href="{{ route('dashboard') }}" class="navbar-brand">
+                            Easydiner
                         </a>
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
                             <i class="fa fa-bars"></i>
@@ -39,11 +39,11 @@
                     <!-- /.navbar-collapse -->
             @else
             <!-- Logo -->
-            <a href="{{ url(config('adminlte.dashboard_url', 'home')) }}" class="logo">
+            <a href="{{ route('dashboard') }}" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini">{!! config('adminlte.logo_mini', '<b>A</b>LT') !!}</span>
+                <span class="logo-mini">Easydiner</span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg">{!! config('adminlte.logo', '<b>Admin</b>LTE') !!}</span>
+                <span class="logo-lg"><b>Easydiner</b></span>
             </a>
 
             <!-- Header Navbar -->
